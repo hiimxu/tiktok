d6b021bf01d65cc1af2d7295ef146d9329540389
HieuDD <mhoguty0213@gmail.com>
2022-09-05 21:19:54 +0700

    done default layout

--- src/components/Layout/HeaderOnly/index.jsx
+++ src/components/Layout/HeaderOnly/index.jsx
@@ -2,7 +2,7 @@ import Header from '~/components/Layout/components/Header';
 
 import React from 'react';
 
-export default function DefaultLayout({ children }) {
+export default function HeaderOnly({ children }) {
   return (
     <div>
       <Header />
