854a4d6eb31499d5e1d9735988657cc3808b79b1
HieuDD <mhoguty0213@gmail.com>
2022-09-05 21:19:54 +0700

    basic layouts

---
+++ src/routes/index.jsx
@@ -0,0 +1,30 @@
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+//Layout
+import { HeaderOnly } from '~/components/Layout';
+
+export const publicRoutes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/following',
+    component: Following,
+  },
+  {
+    path: '/upload',
+    component: Upload,
+    layout: HeaderOnly,
+  },
+  {
+    path: '/search',
+    component: Search,
+    layout: null,
+  },
+];
+
+export const privateRoutes = [];
