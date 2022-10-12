b59bb73c5502e27c9f6455339c4990086fcf4cfa
Xu <mhoguty0213@gmail.com>
2022-10-12 19:57:05 +0700

    Add project WSL

--- src/layouts/components/Sidebar/index.jsx
+++ src/layouts/components/Sidebar/index.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
@@ -65,3 +66,72 @@ export default function Sidebar() {
         </aside>
     );
 }
+=======
+import React from 'react';
+import { useState, useEffect } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import Menu from './Menu';
+import MenuItem from './Menu/MenuItem';
+import config from '~/config';
+import {
+    HomeActiveIcon,
+    HomeIcon,
+    LiveActiveIcon,
+    LiveIcon,
+    UserGroupActiveIcon,
+    UserGroupIcon,
+} from '~/components/Icons';
+import SuggestedAccounts from '~/components/SuggestedAccounts';
+
+//Service
+import * as userService from '~/Services/userService';
+
+const cx = classNames.bind(styles);
+
+const INIT_PAGE = 1;
+const PER_PAGE = 5;
+
+export default function Sidebar() {
+    const [suggestUsers, setSuggestedUsers] = useState([]);
+    const [page, setPage] = useState(INIT_PAGE);
+
+    useEffect(() => {
+        userService
+            .getSuggested({ page: page, perPage: PER_PAGE })
+            .then((data) => {
+                console.log(data);
+                setSuggestedUsers((prev) => [...prev, ...data]);
+            })
+
+            .catch((error) => console.log(error));
+    }, [page]);
+
+    return (
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem
+                    title="For You"
+                    to={config.routes.home}
+                    icon={<HomeIcon />}
+                    activeIcon={<HomeActiveIcon />}
+                />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem
+                    title="LIVE"
+                    to={config.routes.live}
+                    icon={<LiveIcon />}
+                    activeIcon={<LiveActiveIcon />}
+                />
+            </Menu>
+            <SuggestedAccounts label="Suggested accounts" data={suggestUsers} />
+            <SuggestedAccounts label="Following accounts" />
+        </aside>
+    );
+}
+>>>>>>> b236c1a (Add project WSL)
